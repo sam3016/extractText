2a99954ef95b4d3cc2daf1f71cff0304608426ed
Sam.H <samhui3016@gmail.com>
2023-04-07 01:17:26 +0900

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
-//Here we're importing items we'll need. You can add other imports here.
+let pdfjsLib = window['pdfjs-dist/build/pdf'];
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+window.extractText = async (json) => {
+  let blob = dataURItoBlob("data:application/pdf;base64, " + json);
+  let objectURL = URL.createObjectURL(blob);
+  const result = await getItems(objectURL);
+  FileMaker.PerformScriptWithOption("GetResult", result, 5);
+}
 
-//
+async function getContent(src) {
+  const doc = await pdfjsLib.getDocument(src).promise;
+  const page = await doc.getPage(1);
+  return await page.getTextContent();
+}
+
+async function getItems(src) {
+  // Perform checks
+  const content = await getContent(src);
+  /**
+   * Expect content.items to have the following structure
+   * [{
+   *  str: '1',
+   *  dir: 'ltr',
+   *  width: 4.7715440000000005,
+   *  height: 9.106,
+   *  transform: [ 9.106, 0, 0, 9.106, 53.396, 663.101 ],
+   *  fontName: 'g_d0_f2'
+   * }, ...]
+   */
+
+  // you can do the following on content.items
+  return content.items.map((item) => item.str);
+  // This is a new array of strings from the str property
+  // Expected output: ['1', '06/02/2013', '$1,500.00', 'job 1, check 1', 'yes', 'deposit',...]
+}
+
+function dataURItoBlob(dataURI)
+{
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    let byteString;
+
+    if(dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    let ia = new Uint8Array(byteString.length);
+    for(let i = 0; i < byteString.length; i++)
+    {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type: mimeString});
+}
